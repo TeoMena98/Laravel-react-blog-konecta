e9bb5a6ee32ec45ca9d89097199f6750b865e12b
Mateo Ramírez Mena <mateomena10@gmail.com>
2021-04-02 04:36:01 -0500

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+
+class User extends Authenticatable 
 {
    use Notifiable, HasRoles;
 
@@ -35,18 +35,5 @@ class User extends Authenticatable implements JWTSubject
      'email_verified_at' => 'datetime',
    ];
 
-   public function getJWTIdentifier()
-   {
-       return $this->getKey();
-   }
-
-   /**
-    * Return a key value array, containing any custom claims to be added to the JWT.
-    *
-    * @return array
-    */
-   public function getJWTCustomClaims()
-   {
-       return [];
-   }
+  
 }
